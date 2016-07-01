df25ecc2463be5b750065b372dc57ac19da6020c
Peter Vanya <peter.vanya@gmail.com>
2016-07-01 15:32:50 +0100

    Polish, added Numba option

--- gen_input.py
+++ gen_input.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Usage:
-    gen_input.py [--L <L> --dt <dt> --steps <N> --kBT <T> --thermo <th>]
+    gen_input.py [--L <L> --dt <dt> --steps <N> --kT <kT> --thermo <th>]
                  [--NA <NA> --NB <NB> --aii <aii> --aij <aij>]
 
 Generate input for the simulation.
@@ -9,7 +9,7 @@ Options:
     --L <L>        Box size [default: 5]
     --dt <dt>      Time step [default: 0.02]
     --steps <N>    Number of time steps [default: 100]
-    --kBT <T>      Temperature in DPD units [default: 1.0]
+    --kT <kT>      Temperature in DPD units [default: 1.0]
     --NA <NA>      Number of A beads [default: 200]
     --NB <NB>      Number of B beads [default: 200]
     --aii <aii>    Same bead interaction [default: 25]
@@ -24,7 +24,7 @@ args = docopt(__doc__)
 L = float(args["--L"])
 dt = float(args["--dt"])
 steps = int(args["--steps"])
-kBT = float(args["--kBT"])
+kT = float(args["--kT"])
 gamma = 4.5
 rc = 1.0
 thermo = int(args["--thermo"])
@@ -33,13 +33,14 @@ NB = int(args["--NB"])
 aii = float(args["--aii"])
 aij = float(args["--aij"])
 saveE = False
+use_numba = False
 
 
 s = "# Input file for home-made DPD simulation\n"
 s += "L: %.1f\n" % L
 s += "dt: %.2f\n" % dt
 s += "num-steps: %i\n" % steps
-s += "kBT: %.1f\n" % kBT
+s += "kT: %.1f\n" % kT
 s += "gamma: %.1f\n" % gamma
 s += "rc: %.1f\n" % rc
 s += "thermo: %i        # print coords this many times\n" % thermo
@@ -53,6 +54,7 @@ s += "    A A: %.1f\n" % aii
 s += "    A B: %.1f\n" % aij
 s += "    B B: %.1f\n" % aii
 s += "save-energy: %r\n" % saveE
+s += "use-numba: %r\n" % use_numba
 
 open("input.yaml", "w").write(s)
 print("File written in input.yaml")
