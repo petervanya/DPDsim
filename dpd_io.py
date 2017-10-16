6f3d85ba402a285ccb431fdac71837d39c759c47
Peter Vanya <peter.vanya@gmail.com>
2017-10-16 12:54:48 +0100

    Added functions

--- dpd_io.py
+++ dpd_io.py
@@ -4,11 +4,12 @@ Input-output routines
 
 """
 import numpy as np
+import sys
 
 
-def save_xyzmatrix(fname, names, mat):
+def save_xyzfile(fname, names, mat):
     """Save a coordinate matrix in a xyz format"""
-    M = mat.shape[0]
+    M = len(mat)
     s = "%i\nbla\n" % M
     for i in range(M):
         s += "%i\t%.3f\t%.3f\t%.3f\n" % \
@@ -16,8 +17,24 @@ def save_xyzmatrix(fname, names, mat):
     open(fname, "w").write(s)
 
 
-def read_xyzmatrix(fname):
-    A = open(fname, "r").readlines()[2:]
+def read_xyzfile(fname):
+    try:
+        A = open(fname, "r").readlines()[2:]
+    except FileNotFoundError:
+        sys.exit("File %s not found." % fname)
     A = [line.split() for line in A]
     A = np.array(A).astype(float)
     return A
+
+
+def parse_box(arg):
+    L = [float(i) for i in arg.split()]
+    if len(L) == 1:
+        box = np.eye(3) * L[0]
+    elif len(L) == 3:
+        box = np.diag(L)
+    else:
+        sys.exit("L should have length 1 or 3.")
+    return box
+
+
