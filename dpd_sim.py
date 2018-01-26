952e5a759b9c832fe982e32eddf4f447edea9c7c
Peter Vanya <peter.vanya@gmail.com>
2018-01-26 15:26:27 +0000

    Added MDPD option

--- dpd_sim.py
+++ dpd_sim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Usage:
-    dpd_sim.py <method> [--L <L> --N <N> --dt <dt> --gamma <g> --seed <s>]
+    dpd_sim.py <method> [--lang <lang>]
+               [--L <L> --N <N> --dt <dt> --gamma <g> --seed <s>]
                [--steps <ns> --eq <eq> --thermo <th> --style <st>]
                [--dump-freq <df> --dump-vel --dump-for]
                [--xyz <xyz> --vel <vel>]
@@ -8,16 +9,17 @@
 Simulate a DPD binary mixture.
 
 Options:
-    <method>           "numba" or "fortran"
+    <method>           "dpd" or "mdpd"
 
 Options:
+    --lang <lang>      "fortran" or "numba" [default: fortran]
     --N <N>            Number of atoms [default: 375]
     --L <L>            Box size [default: 5]
     --dt <dt>          Timestep [default: 0.05]
     --steps <ns>       Number of steps [default: 100]
     --eq <eq>          Equilibration steps [default: 0]
     --thermo <th>      Frequency of printing to screen [default: 10]
-    --dump-freq <df>   Frequency of printing to file [default: 10]
+    --dump-freq <df>   Frequency of printing to file [default: 100]
     --gamma <g>        Friction [default: 4.5]
     --seed <s>         Random seed [default: 1234]
     --style <st>       Integration style [default: verlet]
@@ -37,6 +39,7 @@ from dpd_io import read_xyzfile, parse_box
 if __name__ == "__main__":
     args = docopt(__doc__)
     np.random.seed(int(args["--seed"]))
+    mt = args["<method>"].lower()
     N = int(args["--N"])
     box = parse_box(args["--L"])
     rho = float(N / np.prod(np.diag(box)))
@@ -47,10 +50,14 @@ if __name__ == "__main__":
     Neq = int(args["--eq"])
     thermo = int(args["--thermo"])
     df = int(args["--dump-freq"])
-    style = args["--style"]
+    style = args["--style"].lower()
     styles = ["euler", "verlet"]
-    if style.lower() not in styles:
+    if style not in styles:
         sys.exit("Choose style from %s." % styles)
+    lng = args["--lang"].lower()
+    lngs = ["numba", "fortran"]
+    if lng not in lngs:
+        sys.exit("Choose lang from 'fortran', 'numba'.")
 
     dumpdir = "Dump"
     if not os.path.exists(dumpdir): os.makedirs(dumpdir)
@@ -80,24 +87,37 @@ if __name__ == "__main__":
         V = np.random.randn(N, 3) * kT
         V = V - np.sum(V, 0) / N
 
+    if mt == "mdpd": # temporary solution
+        ip = np.array([[-40.0]])
+        ipb = np.array([[25.0]])
+        rd = 0.75
+    Ntp = len(ip)
+
+
     print("===== DPD simulation =====")
     print("N: %i | L: %s | rho: %.2f" % (N, np.diag(box), rho))
     print("gamma: %.2f | kT: %.1f | dt: %.3f" % (gamma, kT, dt))
     print("Steps: %i | Neq: %i | Thermo: %i | dump-freq: %i" \
             % (Nt, Neq, thermo, df))
-    print("Integration: %s" % style)
+    print("Code: %s | Integration: %s" % (lng, style))
 
     ti = time.time()
-    mt = args["<method>"].lower()
-    if mt == "numba":
-        from dpd_int import integrate_numba
+    if lng == "numba" and mt == "dpd":
+        from dpd_integrate import integrate_numba
         T, KE, PE = integrate_numba(X, V, bl, ip, box, gamma, kT, dt, \
                 Nt, Neq, thermo, df, dump_vel, dump_for, style)
-    elif mt == "fortran":
-        from dpd_int import integrate_f
+    elif lng == "fortran" and mt == "dpd":
+        from dpd_integrate import integrate_f
         X = np.asfortranarray(X)
         V = np.asfortranarray(V)
         T, KE, PE = integrate_f(X, V, bl, ip, box, gamma, kT, dt, \
+                Nt, Neq, thermo, df, dump_vel, dump_for, style)
+    elif lng == "fortran" and mt == "mdpd":
+        from dpd_integrate import integrate_f_mdpd
+        X = np.asfortranarray(X)
+        V = np.asfortranarray(V)
+        T, KE, PE = integrate_f_mdpd(X, V, bl, Ntp, ip, \
+                box, gamma, kT, dt, ipb, rd,\
                 Nt, Neq, thermo, df, dump_vel, dump_for, style)
 
     print("Simulation time: %.2f s." % (time.time() - ti))
